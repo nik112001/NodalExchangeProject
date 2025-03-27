8650dfbc0cf3f9ca41e9bdb579ca375546534c8f
Nikhil Kotta <nkotta@terpmail.umd.edu>
2025-03-27 17:17:23 -0400

    Producer publishing ping

--- src/main/java/com/example/spring_rabbitmq/SpringRabbitmqApplication.java
+++ src/main/java/com/example/spring_rabbitmq/SpringRabbitmqApplication.java
@@ -1,13 +1,32 @@
 package com.example.spring_rabbitmq;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.amqp.core.Queue;
 
 @SpringBootApplication
 public class SpringRabbitmqApplication {
+
+	private static final String QUEUE_NAME = "testQueue";
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRabbitmqApplication.class, args);
 	}
 
+	@Bean
+	Queue queue() {
+		return new Queue(QUEUE_NAME, false);
+	}
+
+	@Bean
+	CommandLineRunner sendPing(RabbitTemplate rabbitTemplate) {
+		return args -> {
+			System.out.println("Service-1: Sending 'ping'...");
+			rabbitTemplate.convertAndSend(QUEUE_NAME, "ping");
+		};
+	}
 }
